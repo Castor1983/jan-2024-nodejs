445e8f18351a4155010007c49caf1e39e6093ed9
Castor1983 <a0958086750@gmail.com>
2024-07-16 11:58:07 +0300

    CRUD complete

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const express = require('express')
 const app = express()
-const {users} = require('./Users')
+const {read, write} = require('./fs.service')
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-app.get('/users', (req, res) =>{
+app.get('/users', async (req, res) =>{
     try {
+        const users = await read();
         res.json(users)
     }catch (e) {
-        res.status(400).json(e.message)
+        res.status(500).json(e.message)
     }
 
 })
-app.post('/users', (req, res) =>{
+app.post('/users', async (req, res) =>{
     try {
         const {id, name, email,  age} = req.body
+        const users = await read();
         const index = users.findIndex((user) =>user.email === email)
         if (index !== -1) {
             return res.status(409).json('User with this email already exists')
@@ -26,48 +28,57 @@ app.post('/users', (req, res) =>{
             age
         }
         users.push(newUser)
+        await write(users)
+
         res.status(201).json(newUser)
     }catch (e) {
-        res.status(400).json(e.message)
+        res.status(500).json(e.message)
     }
 
 })
-app.get('/users/:userId', (req, res) =>{
+app.get('/users/:userId', async (req, res) =>{
     try {
-       const user = users.find(user => user.id === +req.params.userId)
+        const users = await read();
+        const user = users.find(user => user.id === +req.params.userId)
        if(!user) {
            return res.status(404).json('User not found')
        }
         res.json(user)
     }catch (e) {
-        res.status(400).json(e.message)
+        res.status(500).json(e.message)
     }
 })
-app.put('/users/:userId', (req, res) =>{
+app.put('/users/:userId', async (req, res) =>{
     try {
-        const user = users.find(user => user.id === +req.params.userId)
         const {name, email,  age} = req.body
+        const users = await read();
+        const user = users.find(user => user.id === +req.params.userId)
         if(!user) {
             return res.status(404).json('User not found')
         }
+
        if (name) user.name = name;
        if (email) user.email = email;
        if (age) user.age = age;
-        res.json(user)
+        await write(users);
+        res.status(201).json(user)
+
     }catch (e) {
-        res.status(400).json(e.message)
+        res.status(500).json(e.message)
     }
 })
-app.delete('/users/:userId', (req, res) =>{
+app.delete('/users/:userId', async (req, res) =>{
     try {
-        const index = users.findIndex((user) =>user.id=== +req.params.userId)
-        if (index !== -1) {
+        const users = await read();
+        const index = users.findIndex(user => user.id === +req.params.userId)
+        if (index === -1) {
             return res.status(404).json('User not found')
         }
         users.splice(index, 1);
+        await write(users)
         res.sendStatus(204);
     }catch (e) {
-        res.status(400).json(e.message)
+        res.status(500).json(e.message)
     }
 })
 app.listen(3000, ()=>{
